b16facca0777d504ea6f9c91d26a07fed0f505e6
DaidoujiChen <daidoujichen@gmail.com>
2015-08-21 16:59:54 +0800

    Initial Commit

---
+++ DaiDebugLog/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  DaiDebugLog
+//
+//  Created by DaidoujiChen on 2015/8/21.
+//  Copyright (c) 2015年 DaidoujiChen. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ DaiDebugLog/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  DaiDebugLog
+//
+//  Created by DaidoujiChen on 2015/8/21.
+//  Copyright (c) 2015年 DaidoujiChen. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
